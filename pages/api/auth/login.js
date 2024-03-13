7a91f63f6b8c7d572c713ba353d984320e5899d1
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2024-03-13 17:19:43 +0530

    login issue

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,4 +1,102 @@
-// pages/api/login.js
+// // pages/api/login.js
+
+// import mysql from 'mysql2/promise';
+// import { compareSync } from 'bcrypt';
+// import { sign } from 'jsonwebtoken';
+// import dbConfig from '../../../assets/database/db';
+
+// export default async function handler(req, res) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ message: 'Method Not Allowed' });
+//   }
+
+//   const { email, password } = req.body;
+
+//   console.log(email, password);
+
+//   if (!email || !password) {
+//     return res.status(400).json({ message: 'Email and password are required' });
+//   }
+
+//   let connection;
+//   try {
+//     connection = await mysql.createConnection(dbConfig);
+
+//     const [rows] = await connection.execute(
+//       'SELECT * FROM users WHERE email = ?',
+//       [email]
+//     );
+
+//     if (rows.length === 0) {
+//       return res.status(401).json({ message: 'Invalid email or password' });
+//     }
+
+//     const user = rows[0];
+
+//     if (!compareSync(password, user.password)) {
+//       return res.status(401).json({ message: 'Invalid email or password' });
+//     }
+
+//     if (user.rollno != 0) {
+//       const [student] = await connection.execute(
+//         'SELECT * FROM students WHERE rollno = ? AND active = 1;',
+//         [user.rollno]
+//       );
+
+//       if (student[0]) {
+//         const token = sign(
+//           { userId: user.id, userEmail: user.email },
+//           'havefuneveryone',
+//           {
+//             expiresIn: '365d',
+//           }
+//         );
+
+//         await connection.end();
+
+//         user.password = undefined;
+
+//         res
+//           .status(200)
+//           .json({ user: { ...student[0], email: user.email }, token });
+//       } else {
+//         const token = sign(
+//           { userId: user.id, userEmail: user.email },
+//           'havefuneveryone',
+//           {
+//             expiresIn: '365d',
+//           }
+//         );
+
+//         await connection.end();
+
+//         user.password = undefined;
+
+//         res.status(200).json({ user, token });
+//       }
+//     }
+//     const token = sign(
+//       { userId: user.id, userEmail: user.email },
+//       'havefuneveryone',
+//       {
+//         expiresIn: '365d',
+//       }
+//     );
+
+//     await connection.end();
+
+//     user.password = undefined;
+
+//     res.status(200).json({ user, token });
+//   } catch (error) {
+//     console.log('error');
+//     console.log(error);
+//     console.error('MySQL error:', error);
+//     await connection.end();
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+//   res.status(500).json({ message: 'Method not allowed' });
+// }
 
 import mysql from 'mysql2/promise';
 import { compareSync } from 'bcrypt';
@@ -52,29 +150,14 @@ export default async function handler(req, res) {
           }
         );
 
-        await connection.end();
-
         user.password = undefined;
 
-        res
+        return res
           .status(200)
           .json({ user: { ...student[0], email: user.email }, token });
-      } else {
-        const token = sign(
-          { userId: user.id, userEmail: user.email },
-          'havefuneveryone',
-          {
-            expiresIn: '365d',
-          }
-        );
-
-        await connection.end();
-
-        user.password = undefined;
-
-        res.status(200).json({ user, token });
       }
     }
+
     const token = sign(
       { userId: user.id, userEmail: user.email },
       'havefuneveryone',
@@ -83,17 +166,15 @@ export default async function handler(req, res) {
       }
     );
 
-    await connection.end();
-
     user.password = undefined;
 
     res.status(200).json({ user, token });
   } catch (error) {
-    console.log('error');
-    console.log(error);
     console.error('MySQL error:', error);
-    await connection.end();
     res.status(500).json({ message: 'Internal Server Error' });
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
   }
-  res.status(500).json({ message: 'Method not allowed' });
 }
